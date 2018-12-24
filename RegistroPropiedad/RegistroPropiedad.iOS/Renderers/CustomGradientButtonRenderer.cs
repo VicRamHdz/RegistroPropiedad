78d632bead3f896ba2f30920a664054017361758
Victor Manuel Ramos Hernandez <>
2018-12-24 14:25:44 -0600

    changing language to spanish

--- RegistroPropiedad/RegistroPropiedad.iOS/Renderers/CustomGradientButtonRenderer.cs
+++ RegistroPropiedad/RegistroPropiedad.iOS/Renderers/CustomGradientButtonRenderer.cs
@@ -1,10 +1,72 @@
 ﻿using System;
+using System.Linq;
+using CoreAnimation;
+using CoreGraphics;
+using RegistroPropiedad.CustomControls;
+using RegistroPropiedad.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomGradientButton), typeof(CustomGradientButtonRenderer))]
 namespace RegistroPropiedad.iOS.Renderers
 {
-    public class CustomGradientButtonRenderer
+    public class CustomGradientButtonRenderer : ButtonRenderer
     {
-        public CustomGradientButtonRenderer()
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
         {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                var formsButton = Element as CustomGradientButton;
+                if (formsButton != null)
+                {
+                    SetGradientColor(formsButton);
+                }
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (Element == null) return;
+
+            if (e.PropertyName == "StartColor" || e.PropertyName == "EndColor" || e.PropertyName == "AlphaMultiplier"
+                || e.PropertyName == "HasBorder")
+            {
+                var formsButton = Element as CustomGradientButton;
+                if (formsButton != null)
+                {
+                    SetGradientColor(formsButton);
+                }
+            }
+        }
+
+        private void SetGradientColor(CustomGradientButton element)
+        {
+            var gradientLayer = new CAGradientLayer();
+            gradientLayer.CornerRadius = element.BorderRadius;
+            gradientLayer.Frame = Bounds;
+            gradientLayer.Colors = new CGColor[] { element.StartColor.MultiplyAlpha(element.AlphaMultiplier).ToCGColor(), element.EndColor.MultiplyAlpha(element.AlphaMultiplier).ToCGColor() };
+            if (element.HasBorder)
+            {
+                gradientLayer.BorderWidth = (nfloat)element.BorderWidth;
+                gradientLayer.BorderColor = element.BorderColor.ToCGColor();//element.EndColor.ToCGColor();
+            }
+            var layer = Control?.Layer.Sublayers.LastOrDefault();
+            Control?.Layer.InsertSublayerBelow(gradientLayer, layer);
+            Control?.SetTitleColor(element.TextColor.ToUIColor(), UIControlState.Disabled);
+        }
+
+        public override void LayoutSubviews()
+        {
+            var newBounds = Element.Bounds.ToRectangleF();
+            foreach (var layer in Control?.Layer.Sublayers.Where(layer => layer is CAGradientLayer))
+            {
+                layer.Frame = new CGRect(0, 0, newBounds.Width, newBounds.Height);
+            }
+            base.LayoutSubviews();
         }
     }
 }
--- RegistroPropiedad/RegistroPropiedad/Controles/CustomGradientButton.cs
+++ RegistroPropiedad/RegistroPropiedad/Controles/CustomGradientButton.cs
@@ -1,8 +1,75 @@
 ﻿using System;
-namespace RegistroPropiedad.Controles
+using Xamarin.Forms;
+
+namespace RegistroPropiedad.CustomControls
 {
-    public class CustomGradientButton
+    public class CustomGradientButton : Button
     {
+        public static readonly BindableProperty StartColorProperty =
+        BindableProperty.Create(nameof(StartColor), typeof(Color), typeof(CustomGradientButton),
+                                Color.Transparent, BindingMode.OneWay);
+
+        public Color StartColor
+        {
+            get
+            {
+                return (Color)GetValue(StartColorProperty);
+            }
+            set
+            {
+                SetValue(StartColorProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty EndColorProperty =
+        BindableProperty.Create(nameof(EndColor), typeof(Color), typeof(CustomGradientButton),
+                                Color.Transparent, BindingMode.OneWay);
+
+        public Color EndColor
+        {
+            get
+            {
+                return (Color)GetValue(EndColorProperty);
+            }
+            set
+            {
+                SetValue(EndColorProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty AlphaMultiplierProperty =
+         BindableProperty.Create(nameof(AlphaMultiplier), typeof(double),
+                                 typeof(CustomGradientButton), 1.0D, BindingMode.OneWay);
+
+        public double AlphaMultiplier
+        {
+            get
+            {
+                return (double)GetValue(AlphaMultiplierProperty);
+            }
+            set
+            {
+                SetValue(AlphaMultiplierProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty HasBorderProperty =
+            BindableProperty.Create(nameof(HasBorder), typeof(bool),
+                                    typeof(CustomGradientButton), false, BindingMode.OneWay);
+
+
+        public bool HasBorder
+        {
+            get
+            {
+                return (bool)GetValue(HasBorderProperty);
+            }
+            set
+            {
+                SetValue(HasBorderProperty, value);
+            }
+        }
+
         public CustomGradientButton()
         {
         }
